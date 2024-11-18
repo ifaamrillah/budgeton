cef732986086c6150014325b4f82da50594f1cca
ifaamrillah <amrillah.work@gmail.com>
2024-11-18 21:26:50 +0700

    feat: refactor: create & get all account api

--- src/app/api/account/route.ts
+++ src/app/api/account/route.ts
@@ -1,30 +1,51 @@
+import { NextRequest, NextResponse } from "next/server";
 import { currentUser } from "@clerk/nextjs/server";
+
+import { AccountValidator } from "@/validator/account-validator";
 import { db } from "@/lib/db";
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
+  // Check Authorization
   const user = await currentUser();
   if (!user) {
-    return Response.json(
-      {
-        message:
-          "Unauthorized access. Please check your permissions or log in.",
-      },
+    return NextResponse.json(
+      { message: "Unauthorized access." },
       { status: 401 }
     );
   }
 
-  const url = new URL(req.url);
-  const pageIndex = parseInt(
-    url.searchParams.get("pagination[pageIndex]") || "1"
-  );
-  const pageSize = parseInt(
-    url.searchParams.get("pagination[pageSize]") || "10"
-  );
-  const sortBy = url.searchParams.get("sorting[sortBy]") || undefined;
-  const sortDesc = url.searchParams.get("sorting[sortDesc]") === "true";
+  // Check Synchronization
+  const existingUser = await db.user.findUnique({
+    where: { id: user.id },
+  });
+  if (!existingUser) {
+    return NextResponse.json(
+      { message: "Users are not synchronized" },
+      { status: 422 }
+    );
+  }
 
+  // Check Expired Plan
+  if (existingUser.expiredPlan && existingUser.plan !== "LIFETIME") {
+    const expiredDate = new Date(existingUser.expiredPlan);
+    const currentDate = new Date();
+    if (expiredDate < currentDate) {
+      return NextResponse.json(
+        { message: "User plan has expired." },
+        { status: 403 }
+      );
+    }
+  }
+
+  // Search Params
+  const searchParams = req.nextUrl.searchParams;
+  const pageIndex = parseInt(searchParams.get("pagination[pageIndex]") || "1");
+  const pageSize = parseInt(searchParams.get("pagination[pageSize]") || "10");
+  const sortBy = searchParams.get("sorting[sortBy]") || undefined;
+  const sortDesc = searchParams.get("sorting[sortDesc]") === "true";
   const orderBy = sortBy ? { [sortBy]: sortDesc ? "desc" : "asc" } : undefined;
 
+  // Get All Account
   const getAll = await db.account.findMany({
     where: {
       userId: user.id,
@@ -32,26 +53,20 @@ export async function GET(req: Request) {
     skip: (pageIndex - 1) * pageSize,
     take: pageSize,
     orderBy: orderBy,
-    select: {
-      id: true,
-      name: true,
-      startingBalance: true,
-      status: true,
-    },
   });
 
+  // Pagination
   const totalData = await db.account.count({
     where: {
       userId: user.id,
     },
   });
-
   const totalPage = Math.ceil(totalData / pageSize);
 
   if (getAll.length > 0) {
-    return Response.json(
+    return NextResponse.json(
       {
-        message: "Get All Account Successfully.",
+        message: "Get all account successfully.",
         data: getAll,
         pagination: {
           pageIndex,
@@ -59,7 +74,7 @@ export async function GET(req: Request) {
           totalPage,
           totalData,
         },
-        sorter: {
+        sorting: {
           sortBy,
           sortDesc,
         },
@@ -69,7 +84,7 @@ export async function GET(req: Request) {
   }
 
   if (getAll.length <= 0) {
-    return Response.json(
+    return NextResponse.json(
       {
         message: "No accounts found.",
         data: [],
@@ -79,7 +94,7 @@ export async function GET(req: Request) {
           totalPage: 0,
           totalData: 0,
         },
-        sorter: {
+        sorting: {
           sortBy,
           sortDesc,
         },
@@ -88,9 +103,102 @@ export async function GET(req: Request) {
     );
   }
 
-  return Response.json(
+  return NextResponse.json(
     {
-      message: "Get All Account Failed.",
+      message: "Get all account failed.",
+    },
+    { status: 500 }
+  );
+}
+
+export async function POST(req: Request) {
+  // Check Authorization
+  const user = await currentUser();
+  if (!user) {
+    return NextResponse.json(
+      { message: "Unauthorized access." },
+      { status: 401 }
+    );
+  }
+
+  // Check Synchronization
+  const existingUser = await db.user.findUnique({
+    where: { id: user.id },
+  });
+  if (!existingUser) {
+    return NextResponse.json(
+      { message: "Users are not synchronized" },
+      { status: 422 }
+    );
+  }
+
+  // Check Expired Plan
+  if (existingUser.expiredPlan && existingUser.plan !== "LIFETIME") {
+    const expiredDate = new Date(existingUser.expiredPlan);
+    const currentDate = new Date();
+    if (expiredDate < currentDate) {
+      return NextResponse.json(
+        { message: "User plan has expired." },
+        { status: 403 }
+      );
+    }
+  }
+
+  // Body
+  const body = await req.json();
+  const validatedFields = AccountValidator.safeParse(body);
+  if (!validatedFields.success) {
+    return NextResponse.json(
+      {
+        message:
+          validatedFields?.error?.errors[0]?.message ||
+          "Field validation error",
+      },
+      { status: 400 }
+    );
+  }
+
+  // Check name is unique
+  const checkAccountName = await db.account.findUnique({
+    where: {
+      userId_name: {
+        userId: user.id,
+        name: body.name,
+      },
+    },
+  });
+  if (checkAccountName) {
+    return NextResponse.json(
+      {
+        message: `Account with name: "${body.name}" already exists.`,
+      },
+      { status: 409 }
+    );
+  }
+
+  // Create Account
+  const create = await db.account.create({
+    data: {
+      name: body.name,
+      status: body.status,
+      startingBalance: body.startingBalance,
+      userId: user.id,
+    },
+  });
+  if (create) {
+    return NextResponse.json(
+      {
+        message: "Create account successfully.",
+        data: create,
+      },
+      { status: 201 }
+    );
+  }
+
+  // Internal Server Error
+  return NextResponse.json(
+    {
+      message: "Create account failed.",
     },
     { status: 500 }
   );
