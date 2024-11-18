4c4a34e9953106a4302662fdef7b74c5c6438457
ifaamrillah <amrillah.work@gmail.com>
2024-11-18 21:48:24 +0700

    refactor: delete handle redirect error

--- src/app/sync/page.tsx
+++ src/app/sync/page.tsx
@@ -4,26 +4,16 @@ import { redirect } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 
 import { syncAuth } from "@/services/auth";
+
 import { Spinner } from "@/components/ui/spinner";
 
 export default function Syncpage() {
-  const { data } = useQuery({
+  const { isSuccess } = useQuery({
     queryKey: ["sync"],
     queryFn: syncAuth,
   });
 
-  if (data) {
-    switch (data?.status) {
-      case 201:
-        redirect("/dashboard");
-      case 401:
-        redirect("/sign-in");
-      case 409:
-        redirect("/dashboard");
-      default:
-        redirect("/sign-up");
-    }
-  }
+  if (isSuccess) redirect("/dashboard");
 
   return (
     <div className="flex items-center justify-center w-full h-screen">
