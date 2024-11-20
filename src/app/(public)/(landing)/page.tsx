6791b20709598b38231fe3711380313c661386b4
ifaamrillah <amrillah.work@gmail.com>
2024-11-20 08:36:00 +0700

    feat: add auto redirect to dashboard when login

--- src/app/(public)/(landing)/page.tsx
+++ src/app/(public)/(landing)/page.tsx
@@ -1,3 +1,9 @@
-export default function LandingPage() {
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function LandingPage() {
+  const user = await currentUser();
+  if (user) redirect("/dashboard");
+
   return <main>hallo</main>;
 }
