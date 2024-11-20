ac952b0c0fb94036d5b360b5a743df8a6c81287e
ifaamrillah <amrillah.work@gmail.com>
2024-11-20 09:28:57 +0700

    feat: add income validator

--- src/validator/account-validator.ts
+++ src/validator/account-validator.ts
@@ -5,9 +5,19 @@ export const AccountValidator = z.object({
     message: "Name is required",
   }),
   startingBalance: z
-    .number({ message: "Starting Balance must be number" })
+    .number({ message: "Starting Balance must be a number" })
     .min(0, { message: "Starting Balance must not be minus" }),
   status: z.boolean(),
 });
 
 export type TypeAccountValidator = z.infer<typeof AccountValidator>;
+
+export const IncomeValidator = z.object({
+  date: z.date({ message: "Date is required" }),
+  description: z.string().optional(),
+  amount: z
+    .number({ message: "Amount must be a number" })
+    .min(0, { message: "Amount must not be minus" }),
+});
+
+export type TypeIncomeValidator = z.infer<typeof IncomeValidator>;
