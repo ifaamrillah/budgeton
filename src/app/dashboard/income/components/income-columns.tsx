7dad176da074a01f7f09ff504762b4c99951b132
ifaamrillah <amrillah.work@gmail.com>
2024-11-22 23:00:45 +0700

    feat: add income columns table

---
+++ src/app/dashboard/income/components/income-columns.tsx
@@ -0,0 +1,111 @@
+import { Income } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { format } from "date-fns";
+import { Settings, SquarePen, Trash2 } from "lucide-react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+export const incomeColumns: ColumnDef<Income>[] = [
+  {
+    accessorKey: "id",
+    header: "No",
+    cell: ({ row }) => row.index + 1,
+    meta: {
+      className: "w-[60px]",
+    },
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+    enableSorting: true,
+    meta: {
+      className: "w-[110px]",
+    },
+    cell: ({ row }) => (
+      <div className="text-end">
+        {format(row.getValue("date"), "d MMM yyyy")}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+    cell: ({ row }) => {
+      const text = row.getValue("description") as string;
+      const maxChar = 50;
+      const ellypsis =
+        text?.length > maxChar ? text.slice(0, maxChar) + "..." : text;
+      return (
+        <Tooltip>
+          <TooltipTrigger className="text-start">{ellypsis}</TooltipTrigger>
+          <TooltipContent className="w-[400px]">{text}</TooltipContent>
+        </Tooltip>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+    enableSorting: true,
+    meta: {
+      className: "w-[200px]",
+    },
+    cell: ({ row }) => {
+      const startingBalance = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+      }).format(startingBalance);
+      return formatted;
+    },
+  },
+  {
+    accessorKey: "account.name",
+    header: "Account",
+    meta: {
+      className: "w-[250px]",
+    },
+  },
+  {
+    id: "actions",
+    header: "Action",
+    meta: {
+      className: "w-[60px]",
+    },
+    cell: () => <ActionButton />,
+  },
+];
+
+const ActionButton = () => {
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <Settings className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem className="cursor-pointer">
+            <SquarePen className="size-4" /> Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem className="text-red-500 cursor-pointer">
+            <Trash2 className="size-4" /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
