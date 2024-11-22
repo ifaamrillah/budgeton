5baa9f756832237e751489affa97cb3609eb6a95
ifaamrillah <amrillah.work@gmail.com>
2024-11-22 23:00:08 +0700

    feat: add get all income service

--- src/services/income.ts
+++ src/services/income.ts
@@ -1,5 +1,12 @@
 import { TypeIncomeValidator } from "@/validator/account-validator";
-import { apiPost } from "@/lib/axiosClient";
+import { apiGet, apiPost } from "@/lib/axiosClient";
+
+export async function getAllIncome(params?: Record<string, unknown>) {
+  return await apiGet({
+    url: "/income",
+    params,
+  });
+}
 
 export async function createIncome(data: TypeIncomeValidator) {
   return await apiPost({
