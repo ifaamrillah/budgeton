56c4b4cf0ba76adcbbf6127ba7a58ae14bd19cc8
ifaamrillah <amrillah.work@gmail.com>
2024-11-23 08:43:47 +0700

    feat: add edit-income-by-id service

--- src/services/income.ts
+++ src/services/income.ts
@@ -1,5 +1,5 @@
 import { TypeIncomeValidator } from "@/validator/account-validator";
-import { apiGet, apiPost } from "@/lib/axiosClient";
+import { apiGet, apiPatch, apiPost } from "@/lib/axiosClient";
 
 export async function getAllIncome(params?: Record<string, unknown>) {
   return await apiGet({
@@ -17,6 +17,13 @@ export async function getIncomeById(id?: string) {
 export async function createIncome(data: TypeIncomeValidator) {
   return await apiPost({
     url: "/income",
+    data,
+  });
+}
+
+export async function updateIncomeById(id: string, data: TypeIncomeValidator) {
+  return await apiPatch({
+    url: `/income/${id}`,
     data,
   });
 }
