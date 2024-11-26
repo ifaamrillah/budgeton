26481e2f31cc5f789db73314a441c81c3fc4d2f0
ifaamrillah <amrillah.work@gmail.com>
2024-11-26 10:07:33 +0700

    feat: add get all expense service

--- src/services/expense-service.ts
+++ src/services/expense-service.ts
@@ -1,5 +1,12 @@
-import { apiPost } from "@/lib/axiosClient";
+import { apiGet, apiPost } from "@/lib/axiosClient";
 import { TypeExpenseValidator } from "@/lib/validator";
+
+export async function getAllExpense(params?: Record<string, unknown>) {
+  return await apiGet({
+    url: "/expense",
+    params,
+  });
+}
 
 export async function createExpense(data: TypeExpenseValidator) {
   return await apiPost({
