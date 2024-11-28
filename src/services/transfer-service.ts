c3cc0a5c0b14029b3e16b06b018efccf2bfe78eb
ifaamrillah <amrillah.work@gmail.com>
2024-11-28 08:22:29 +0700

    feat: add get all transfer service

--- src/services/transfer-service.ts
+++ src/services/transfer-service.ts
@@ -1,5 +1,12 @@
-import { apiPost } from "@/lib/axiosClient";
+import { apiGet, apiPost } from "@/lib/axiosClient";
 import { TypeTransferValidator } from "@/lib/validator";
+
+export async function getAllTransfer(params?: Record<string, unknown>) {
+  return await apiGet({
+    url: "/transfer",
+    params,
+  });
+}
 
 export async function createTransfer(data: TypeTransferValidator) {
   return await apiPost({
