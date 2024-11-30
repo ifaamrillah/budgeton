f06b95a142af0cc3eb26b6f14083285c5d446b94
ifaamrillah <amrillah.work@gmail.com>
2024-11-30 13:54:17 +0700

    feat: add get transfer by id & update transfer by id service

--- src/services/transfer-service.ts
+++ src/services/transfer-service.ts
@@ -1,4 +1,4 @@
-import { apiGet, apiPost } from "@/lib/axiosClient";
+import { apiGet, apiPatch, apiPost } from "@/lib/axiosClient";
 import { TypeTransferValidator } from "@/lib/validator";
 
 export async function getAllTransfer(params?: Record<string, unknown>) {
@@ -8,9 +8,25 @@ export async function getAllTransfer(params?: Record<string, unknown>) {
   });
 }
 
+export async function getTransferById(id?: string) {
+  return await apiGet({
+    url: `/transfer/${id}`,
+  });
+}
+
 export async function createTransfer(data: TypeTransferValidator) {
   return await apiPost({
     url: "/transfer",
+    data,
+  });
+}
+
+export async function updateTransferById(
+  id: string,
+  data: TypeTransferValidator
+) {
+  return await apiPatch({
+    url: `/transfer/${id}`,
     data,
   });
 }
