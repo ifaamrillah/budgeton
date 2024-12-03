015c28743aa45b9d155ea97b9a37aa877bed952a
ifaamrillah <amrillah.work@gmail.com>
2024-12-03 15:24:30 +0700

    feat: add action button edit & delete category component

--- src/app/dashboard/(master)/category/components/category-columns.tsx
+++ src/app/dashboard/(master)/category/components/category-columns.tsx
@@ -1,11 +1,25 @@
+"use client";
+
+import { useState } from "react";
 import { ColumnDef } from "@tanstack/react-table";
+import { Settings, SquarePen, Trash2 } from "lucide-react";
 
 import {
+  ActionColumn,
   DefaultColumn,
   NameColumn,
   NoColumn,
 } from "@/components/table-columns";
 import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+import { CategoryModal } from "./category-modal";
 
 export const categoryColumns: ColumnDef<unknown>[] = [
   NoColumn({
@@ -36,4 +50,45 @@ export const categoryColumns: ColumnDef<unknown>[] = [
     enableSorting: true,
     widthAuto: true,
   }),
+  ActionColumn({
+    accessorKey: "actions",
+    header: "Action",
+    cell: ({ row }) => <ActionButton id={row.getValue("id")} />,
+  }),
 ];
+
+const ActionButton = ({ id }: { id: string }) => {
+  const [isModalEditOpen, setModalEditOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <Settings className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            onClick={() => setModalEditOpen(true)}
+            className="cursor-pointer"
+          >
+            <SquarePen className="size-4" /> Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem className="text-red-500 cursor-pointer">
+            <Trash2 className="size-4" /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {isModalEditOpen && (
+        <CategoryModal
+          id={id}
+          isOpen={isModalEditOpen}
+          setOpen={setModalEditOpen}
+        />
+      )}
+    </>
+  );
+};
